9b30b7ca3f294919b719b5d2312a4acd2f89384a
Michael Kalygin <michael.kalygin@gmail.com>
2023-07-26 19:33:51 +0400

    Implement list-all

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for mariadb.
 GH_REPO="https://github.com/mariadb/server"
 TOOL_NAME="mariadb"
 TOOL_TEST="mariadb --version"
@@ -14,25 +13,18 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if mariadb is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sort -V
 }
 
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -o 'refs/tags/mariadb-\d.*' |
+		cut -d/ -f3- |
+		sed 's/^mariadb-//'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if mariadb has other means of determining installable versions.
 	list_github_tags
 }
 
